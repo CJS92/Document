5f34a7c8439789b4cbd39e35cb299636f67899ea
Cody Sloan <46944830+CJS92@users.noreply.github.com>
2019-02-15 16:02:53 -0600

    Add files via upload
    Work in progress

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 // 02/15/2019
 
 using System;
+using System.IO;
 
 namespace Document
 {
@@ -10,9 +11,38 @@ namespace Document
     {
         public static void Main(string[] args)
         {
+            // display name of file followed by a blank line
             Console.WriteLine("Document");
-            Console.WriteLine("\n");
+            Console.WriteLine("------------------");
             Console.ReadLine();
+
+            // gather user input
+            Console.WriteLine("Enter file name: ");
+            string fileName = Console.ReadLine();
+            if (string.IsNullOrEmpty(Path.GetExtension(fileName)))
+            {
+                fileName += ".txt";
+            }
+            Console.WriteLine("\n------------------");
+            Console.WriteLine("Enter file contents: ");
+
+            // write to text file
+            string[] names = new string[] { "Zara Ali", "Nuha Ali" };
+
+            using (StreamWriter sw = new StreamWriter(fileName));
+            {
+
+                foreach (string s in names)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+            // display output to console
+            Console.Write("\nFile Name: {0}", fileName + Environment.NewLine);
+            Console.WriteLine("------------------");
+            Console.WriteLine("------------------");
+            Console.WriteLine("\nPress Enter to exit...");
+            Console.ReadKey();
         }
     }
 }
