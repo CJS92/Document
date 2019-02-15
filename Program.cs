7234224dd792962b261b55f43be410336f3c1f3c
Cody Sloan <46944830+CJS92@users.noreply.github.com>
2019-02-15 11:04:30 -0600

    Add files via upload
    Work in progress.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Project for IT 2040
+// Written by Cody Sloan
+// 02/15/2019
+
+using System;
+
+namespace Document
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Document");
+            Console.WriteLine("\n");
+            Console.ReadLine();
+        }
+    }
+}
